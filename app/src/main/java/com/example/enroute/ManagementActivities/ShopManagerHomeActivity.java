e441e496e8f77ea9f1c0556f45ee4d7b63cbead1
Polina Berest <pamapolya@gmail.com>
2024-04-29 22:37:38 +0300

    Added authorization
    Problem: java.net.UnknownServiceException: CLEARTEXT communication to 10.0.2.2 not permitted by network security policy

---
+++ app/src/main/java/com/example/enroute/ManagementActivities/ShopManagerHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.enroute.ManagementActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.enroute.R;
+
+public class ShopManagerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_manager_home);
+    }
+}
